9e819651a5f44383707479935067288ca14d741b
Karen Kim <kak003@ucsd.edu>
2015-02-20 21:15:44 +0000

    putting it together lab

--- models.js
+++ models.js
@@ -4,6 +4,10 @@ var Mongoose = require('mongoose');
 
 var ProjectSchema = new Mongoose.Schema({
   // fields are defined here
+  "titile": String,
+  "image": String,
+  "summary": String,
+  "date":Date
 });
 
 exports.Project = Mongoose.model('Project', ProjectSchema);
